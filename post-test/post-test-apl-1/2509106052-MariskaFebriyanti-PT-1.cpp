e7c518f5b88d46942afdd53db508605de4060f03
Mariska0207 <febriantimar@gmail.com>
2026-02-26 21:01:49 +0800

    bismillah100

--- post-test/post-test-apl-1/2509106052-MariskaFebriyanti-PT-1.cpp
+++ post-test/post-test-apl-1/2509106052-MariskaFebriyanti-PT-1.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 int main(){
+    system("cls");
     int percobaan = 0;
     string ulangi;
-    cout << "------------------------------------------" << endl;
-    cout << "Selamat Datang di Program Konversi Waktu" << endl;
-    cout << "-----------------------------------------" << endl;
+    cout << "=================================================" << endl;
+    cout << "    Selamat Datang di Program Konversi Waktu" << endl;
+    cout << "=================================================" << endl;
+    
     while(percobaan < 3){
         string username;
         string password;
@@ -14,31 +16,39 @@ int main(){
         cin >> username;
         cout << "Masukkan Password : ";
         cin >> password;
+        
         if(username == "mariska" && password == "052"){
             cout << "~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
             cout << "     Login Berhasil" << endl;
             cout << "~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+            system("cls");
             break;
-        }else{
+        } else {
             percobaan++;
             cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
             cout << "     Login anda ke-" << percobaan << " Gagal" << endl;
             cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
+            
             if(percobaan == 3){
                 cout << "Anda telah gagal login sebanyak 3 kali. Program berhenti." << endl;
                 return 0;
             }
         }
     }
+    
     while(true){
         int pilihan;
         float jam, menit, detik;
-        cout << "\npilih waktu yang ingin di konversi : " << endl;
-        cout << "1. Jam" << endl;
-        cout << "2. Menit" << endl;
-        cout << "3. Detik" << endl;
+        cout << "=================================================" << endl;
+        cout << "              MENU KONVERSI WAKTU" << endl;
+        cout << "=================================================" << endl;
+        cout << "1. Jam ke menit dan detik" << endl;
+        cout << "2. Menit ke jam dan detik" << endl;
+        cout << "3. Detik ke jam dan menit" << endl;
+        cout << "=================================================" << endl;
         cout << "Pilihan Anda : ";
         cin >> pilihan;
+        
         if(pilihan == 1){
             cout << "Masukkan nilai waktu yang ingin dikonversi : ";
             cin >> jam;
@@ -47,8 +57,7 @@ int main(){
             cout << "=================================================" << endl;
             cout << "hasil konversi " << jam << " jam , yaitu : " << menit << " menit " << detik << " detik" << endl;
             cout << "=================================================" << endl;
-        }
-        else if(pilihan == 2){
+        } else if(pilihan == 2){
             cout << "Masukkan nilai waktu yang ingin dikonversi : ";
             cin >> menit;
             jam = menit / 60;
@@ -56,8 +65,7 @@ int main(){
             cout << "=================================================" << endl;
             cout << "hasil konversi " << menit << " menit , yaitu : " << jam << " jam " << detik << " detik" << endl;
             cout << "=================================================" << endl;
-        }
-        else if(pilihan == 3){
+        } else if(pilihan == 3){
             cout << "Masukkan nilai waktu yang ingin dikonversi : ";
             cin >> detik;
             jam = detik / 3600;
@@ -71,17 +79,18 @@ int main(){
             cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
             continue;
         }
-
+        
         cout << "\nApakah Anda ingin mengulang konversi? (ya/tidak): ";
         cin >> ulangi;
-
+        
         if (ulangi == "tidak" || ulangi == "t" || ulangi == "Tidak" || ulangi == "T" || ulangi == "TIDAK") {
             cout << "Terima kasih! Program selesai." << endl;
             cout << "(˶ᵔ ᵕ ᵔ˶)" ;
             break;
-        }else if (ulangi == "ya" || ulangi == "y" || ulangi == "Ya" || ulangi == "Y" || ulangi == "YA") {
+        } else if (ulangi == "ya" || ulangi == "y" || ulangi == "Ya" || ulangi == "Y" || ulangi == "YA") {
+            system("cls");
             continue;
-        }else{
+        } else {
             cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
             cout << "     inputan tidak valid" << endl;
             cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
