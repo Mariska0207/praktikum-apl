9f41b93b4d1284bfcdc28dae1129e64ce0234a95
Mariska0207 <febriantimar@gmail.com>
2026-02-26 11:58:13 +0800

    test

--- post-test/post-test-apl-1/2509106052-MariskaFebriyanti-PT-1.cpp
+++ post-test/post-test-apl-1/2509106052-MariskaFebriyanti-PT-1.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
+
 int main(){
     int percobaan = 0;
     string ulangi;
+    cout << "------------------------------------------" << endl;
+    cout << "Selamat Datang di Program Konversi Waktu" << endl;
+    cout << "-----------------------------------------" << endl;
     while(percobaan < 3){
         string username;
         string password;
@@ -11,11 +15,15 @@ int main(){
         cout << "Masukkan Password : ";
         cin >> password;
         if(username == "mariska" && password == "052"){
-            cout << "Login Berhasil" << endl;
+            cout << "~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+            cout << "     Login Berhasil" << endl;
+            cout << "~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
             break;
         }else{
             percobaan++;
-            cout << "Login anda ke-" << percobaan << " Gagal" << endl;
+            cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
+            cout << "     Login anda ke-" << percobaan << " Gagal" << endl;
+            cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
             if(percobaan == 3){
                 cout << "Anda telah gagal login sebanyak 3 kali. Program berhenti." << endl;
                 return 0;
@@ -36,37 +44,48 @@ int main(){
             cin >> jam;
             menit = jam * 60;
             detik = jam * 3600;
+            cout << "=================================================" << endl;
             cout << "hasil konversi " << jam << " jam , yaitu : " << menit << " menit " << detik << " detik" << endl;
+            cout << "=================================================" << endl;
         }
         else if(pilihan == 2){
             cout << "Masukkan nilai waktu yang ingin dikonversi : ";
             cin >> menit;
             jam = menit / 60;
             detik = menit * 60;
+            cout << "=================================================" << endl;
             cout << "hasil konversi " << menit << " menit , yaitu : " << jam << " jam " << detik << " detik" << endl;
+            cout << "=================================================" << endl;
         }
         else if(pilihan == 3){
             cout << "Masukkan nilai waktu yang ingin dikonversi : ";
             cin >> detik;
             jam = detik / 3600;
             menit = detik / 60;
+            cout << "=================================================" << endl;
             cout << "hasil konversi " << detik << " detik , yaitu : " << jam << " jam " << menit << " menit" << endl;
+            cout << "=================================================" << endl;
         } else {
-            cout << "Pilihan tidak valid." << endl;
+            cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
+            cout << "     Pilihan tidak valid." << endl;
+            cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
             continue;
         }
 
         cout << "\nApakah Anda ingin mengulang konversi? (ya/tidak): ";
         cin >> ulangi;
 
-    if (ulangi == "tidak" || ulangi == "t" || ulangi == "Tidak" || ulangi == "T" || ulangi == "TIDAK") {
-        cout << "Terima kasih! Program selesai." << endl;
-        break;
+        if (ulangi == "tidak" || ulangi == "t" || ulangi == "Tidak" || ulangi == "T" || ulangi == "TIDAK") {
+            cout << "Terima kasih! Program selesai." << endl;
+            cout << "(˶ᵔ ᵕ ᵔ˶)" ;
+            break;
         }else if (ulangi == "ya" || ulangi == "y" || ulangi == "Ya" || ulangi == "Y" || ulangi == "YA") {
-        continue;
+            continue;
         }else{
-        cout << "inputan tidak valid";
-        break;
+            cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
+            cout << "     inputan tidak valid" << endl;
+            cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
+            break;
         }
     }
     return 0;
